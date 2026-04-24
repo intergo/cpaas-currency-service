268435ace44dd0ea2fda00241eb2971e7b6fac87
alexandros <alexandros.g@intergotelecom.com>
2026-04-24 19:30:41 +0300

    added currency dto

---
+++ src/main/java/com/intergotelecom/rest/dto/CurrencyDto.java
@@ -0,0 +1,14 @@
+package com.intergotelecom.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyDto {
+    private String currencyName;
+}
