20ff48741dfcca043eb57158761ec8ce038893eb
alexandros <alexandros.g@intergotelecom.com>
2026-04-24 19:23:47 +0300

    added query

--- src/main/java/com/intergotelecom/repository/CurrencyRepository.java
+++ src/main/java/com/intergotelecom/repository/CurrencyRepository.java
@@ -3,7 +3,11 @@ package com.intergotelecom.repository;
 import com.intergotelecom.model.CurrencyEntity;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class CurrencyRepository implements PanacheMongoRepository<CurrencyEntity> {
+    public List<CurrencyEntity> findAvailable() {
+        return list("available", true);
+    }
 }
