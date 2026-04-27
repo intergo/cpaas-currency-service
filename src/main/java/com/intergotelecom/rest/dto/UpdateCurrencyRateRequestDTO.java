bc91e75b1132cafe1e2734b795b5e1f872d11039
alexandros <alexandros.g@intergotelecom.com>
2026-04-27 17:56:39 +0300

    added update currency dto

---
+++ src/main/java/com/intergotelecom/rest/dto/UpdateCurrencyRateRequestDTO.java
@@ -0,0 +1,25 @@
+package com.intergotelecom.rest.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class UpdateCurrencyRateRequestDTO {
+    @NotBlank(message = "Currency name cannot be blank")
+    @JsonProperty("currency_name")
+    private String currencyName;
+
+    @NotNull(message = "Rate cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Rate must be greater than 0")
+    private BigDecimal rate;
+}
