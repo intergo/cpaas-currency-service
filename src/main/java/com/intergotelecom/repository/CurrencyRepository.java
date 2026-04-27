d8ae5ce42719a767c86c1072734854d388a2871c
alexandros <alexandros.g@intergotelecom.com>
2026-04-27 17:32:24 +0300

    added find base currency query

--- src/main/java/com/intergotelecom/repository/CurrencyRepository.java
+++ src/main/java/com/intergotelecom/repository/CurrencyRepository.java
@@ -4,10 +4,15 @@ import com.intergotelecom.model.CurrencyEntity;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class CurrencyRepository implements PanacheMongoRepository<CurrencyEntity> {
     public List<CurrencyEntity> findAvailable() {
-        return list("available", true);
+        return list("available = ?1 and baseCurrency = ?2", true, false);
+    }
+
+    public Optional<CurrencyEntity> findBaseCurrency() {
+        return find("baseCurrency", true).firstResultOptional();
     }
 }
