c3b2db6dc3460241622338c36021517871b3c986
alexandros <alexandros.g@intergotelecom.com>
2026-04-27 17:40:40 +0300

    added base currency check

--- src/main/java/com/intergotelecom/service/CurrencyService.java
+++ src/main/java/com/intergotelecom/service/CurrencyService.java
@@ -8,6 +8,7 @@ import com.intergotelecom.rest.dto.CurrencyListResponseDTO;
 import com.intergotelecom.rest.dto.CurrencyResponseDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @ApplicationScoped
@@ -24,7 +25,7 @@ public class CurrencyService {
             .map(currencyMapper::toResponseDto)
             .toList();
 
-        String baseCurrencyName = currencyRepository.findBaseCurrency()
+        String baseCurrencyName = getBaseCurrencyOptional()
             .map(CurrencyEntity::getCurrencyName)
             .orElse(null);
 
@@ -35,8 +36,19 @@ public class CurrencyService {
     }
 
     public CurrencyResponseDTO createCurrency(CreateCurrencyRequestDTO dto) {
+      // only 1 currency should be set as base
+      if (dto.isBaseCurrency()) {
+        getBaseCurrencyOptional().ifPresent(
+            currencyEntity -> currencyEntity.setBaseCurrency(false));
+      }
+
+      // convert to entity and persist
       CurrencyEntity entity = currencyMapper.toEntity(dto);
       currencyRepository.persist(entity);
       return currencyMapper.toResponseDto(entity);
+    }
+
+    private Optional<CurrencyEntity> getBaseCurrencyOptional() {
+      return currencyRepository.findBaseCurrency();
     }
 }
