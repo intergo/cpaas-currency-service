f5d566bc799fb593796262e99f03ec0b63263204
alexandros <alexandros.g@intergotelecom.com>
2026-04-28 17:33:17 +0300

    update method

--- src/main/java/com/intergotelecom/service/CurrencyRateService.java
+++ src/main/java/com/intergotelecom/service/CurrencyRateService.java
@@ -89,9 +89,11 @@ public class CurrencyRateService {
         return currencyRateMapper.toResponseDto(baseCurrencyName, rateEntities);
     }
 
-    public CurrencyRatesResponseDTO getCurrencyRates(String baseCurrencyName) {
-        List<CurrencyRateEntity> rates = currencyRateRepository.findByBaseCurrency(baseCurrencyName);
-        return currencyRateMapper.toResponseDto(baseCurrencyName, rates);
+    public CurrencyRatesResponseDTO getCurrencyRatesResponse(String baseCurrencyName, List<String> currencyNames) {
+      List<CurrencyRateEntity> rates = currencyRateRepository
+          .findByCurrencyAndBaseCurrency(baseCurrencyName, currencyNames);
+
+      return currencyRateMapper.toResponseDto(baseCurrencyName, rates);
     }
 
     private List<CurrencyRateEntity> getCurrencyRates(String baseCurrency, Set<String> currencyNames) {
