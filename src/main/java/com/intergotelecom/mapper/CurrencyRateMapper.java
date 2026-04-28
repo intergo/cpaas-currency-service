976b249dd4605f82271241de68165840a59a36d5
alexandros <alexandros.g@intergotelecom.com>
2026-04-28 14:36:53 +0300

    updated mapper

--- src/main/java/com/intergotelecom/mapper/CurrencyRateMapper.java
+++ src/main/java/com/intergotelecom/mapper/CurrencyRateMapper.java
@@ -1,21 +1,30 @@
 package com.intergotelecom.mapper;
 
+import com.intergotelecom.model.CurrencyEntity;
 import com.intergotelecom.model.CurrencyRateEntity;
 import com.intergotelecom.rest.dto.CurrencyRateResponseDTO;
-import com.intergotelecom.rest.dto.UpdateCurrencyRateRequestDTO;
-import java.time.LocalDateTime;
-import org.bson.types.ObjectId;
+import com.intergotelecom.rest.dto.UpdateCurrencyRateDTO;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 
-@Mapper(componentModel = ComponentModel.CDI, imports = {ObjectId.class, LocalDateTime.class})
+@Mapper(componentModel = ComponentModel.CDI)
 public interface CurrencyRateMapper {
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "currencyName", source = "currency.currencyName")
+    @Mapping(target = "baseCurrencyName", source = "baseCurrency.currencyName")
     CurrencyRateResponseDTO toResponseDto(CurrencyRateEntity entity);
 
+    List<CurrencyRateResponseDTO> toResponseDto(List<CurrencyRateEntity> entity);
+
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "currencyId", ignore = true)
-    @Mapping(target = "createdAt", expression = "java(LocalDateTime.now())")
-    CurrencyRateEntity toEntity(UpdateCurrencyRateRequestDTO dto);
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "rate", source = "dto.rate")
+    @Mapping(target = "currency", source = "currencyEntity")
+    @Mapping(target = "baseCurrency", source = "baseCurrencyEntity")
+    CurrencyRateEntity toEntity(
+        UpdateCurrencyRateDTO dto,
+        CurrencyEntity currencyEntity,
+        CurrencyEntity baseCurrencyEntity);
 }
