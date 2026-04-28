fedce5d7818b72b155bc02f02eed1b039c1a23c6
alexandros <alexandros.g@intergotelecom.com>
2026-04-28 14:34:24 +0300

    updated mapper

--- src/main/java/com/intergotelecom/mapper/CurrencyMapper.java
+++ src/main/java/com/intergotelecom/mapper/CurrencyMapper.java
@@ -4,12 +4,11 @@ import com.intergotelecom.model.CurrencyEntity;
 import com.intergotelecom.rest.dto.CreateCurrencyRequestDTO;
 import com.intergotelecom.rest.dto.CurrencyResponseDTO;
 import java.time.LocalDateTime;
-import org.bson.types.ObjectId;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 
-@Mapper(componentModel = ComponentModel.CDI, imports = {ObjectId.class, LocalDateTime.class})
+@Mapper(componentModel = ComponentModel.CDI, imports = {LocalDateTime.class})
 public interface CurrencyMapper {
     CurrencyResponseDTO toResponseDto(CurrencyEntity entity);
 
