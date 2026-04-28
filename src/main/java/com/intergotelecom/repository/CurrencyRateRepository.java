17885e2d51f7b9328a926780b5fe3fd915bbfbce
alexandros <alexandros.g@intergotelecom.com>
2026-04-28 14:34:11 +0300

    added query

--- src/main/java/com/intergotelecom/repository/CurrencyRateRepository.java
+++ src/main/java/com/intergotelecom/repository/CurrencyRateRepository.java
@@ -3,7 +3,14 @@ package com.intergotelecom.repository;
 import com.intergotelecom.model.CurrencyRateEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+import java.util.Set;
 
 @ApplicationScoped
 public class CurrencyRateRepository implements PanacheRepository<CurrencyRateEntity> {
+    public List<CurrencyRateEntity> findByCurrencyAndBaseCurrency(
+        String baseCurrency, Set<String> currencyNames) {
+      return list("baseCurrency.currencyName = ?1 and currency.currencyName in ?1",
+          baseCurrency, currencyNames);
+    }
 }
