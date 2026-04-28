ead49de91e9fbdd7e13e31f832be5dfd3bc81a0a
alexandros <alexandros.g@intergotelecom.com>
2026-04-28 18:08:39 +0300

    cache currencies to redis

--- src/main/java/com/intergotelecom/service/CurrencyRateService.java
+++ src/main/java/com/intergotelecom/service/CurrencyRateService.java
@@ -1,20 +1,24 @@
 package com.intergotelecom.service;
 
+import com.intergotelecom.enums.RedisKeys;
 import com.intergotelecom.mapper.CurrencyRateMapper;
 import com.intergotelecom.model.CurrencyEntity;
 import com.intergotelecom.model.CurrencyRateEntity;
 import com.intergotelecom.repository.CurrencyRateRepository;
 import com.intergotelecom.rest.dto.CurrencyRatesResponseDTO;
 import com.intergotelecom.rest.dto.UpdateCurrencyRateDTO;
+import com.intergotelecom.service.dto.CurrencyRedisDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.NotFoundException;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Transactional
 @ApplicationScoped
@@ -28,6 +32,12 @@ public class CurrencyRateService {
 
     private final
     CurrencyService currencyService;
+
+    private final
+    RedisService<CurrencyRedisDTO> redisService;
+
+    @ConfigProperty(name = "currency.rate.cache.ttl", defaultValue = "3600")
+    private long cacheTtlSeconds;
 
     public CurrencyRatesResponseDTO setCurrencyRates(
             String baseCurrencyName, List<UpdateCurrencyRateDTO> currencyRatesDTOs) {
@@ -86,6 +96,10 @@ public class CurrencyRateService {
         // create response and return
         rateEntities.addAll(newRateEntities);
 
+        // cache currency rates to redis
+        List<CurrencyRedisDTO> redisDTOs = currencyRateMapper.toRedisDTO(baseCurrencyName, currencyRatesDTOs);
+        cacheRates(baseCurrencyName, redisDTOs);
+
         return currencyRateMapper.toResponseDto(baseCurrencyName, rateEntities);
     }
 
@@ -99,5 +113,14 @@ public class CurrencyRateService {
     private List<CurrencyRateEntity> getCurrencyRates(String baseCurrency, Set<String> currencyNames) {
       return currencyRateRepository.findByCurrencyAndBaseCurrency(
           baseCurrency, currencyNames);
+    }
+
+    private void cacheRates(String baseCurrencyName, List<CurrencyRedisDTO> dtos) {
+      Duration ttl = Duration.ofSeconds(cacheTtlSeconds);
+
+      dtos.forEach(dto -> {
+        String key = RedisKeys.createCurrencyKey(baseCurrencyName, dto.getCurrency());
+        redisService.cacheObject(key, ttl, dto);
+      });
     }
 }
