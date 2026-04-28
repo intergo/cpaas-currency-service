9772a70da4ab87a276e1de553ea45f88cbf0b421
alexandros <alexandros.g@intergotelecom.com>
2026-04-28 18:08:02 +0300

    added new mappings

--- src/main/java/com/intergotelecom/mapper/CurrencyRateMapper.java
+++ src/main/java/com/intergotelecom/mapper/CurrencyRateMapper.java
@@ -5,6 +5,7 @@ import com.intergotelecom.model.CurrencyRateEntity;
 import com.intergotelecom.rest.dto.CurrencyRateResponseDTO;
 import com.intergotelecom.rest.dto.CurrencyRatesResponseDTO;
 import com.intergotelecom.rest.dto.UpdateCurrencyRateDTO;
+import com.intergotelecom.service.dto.CurrencyRedisDTO;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -20,6 +21,15 @@ public interface CurrencyRateMapper {
     @Mapping(target = "baseCurrencyName", source = "baseCurrency")
     @Mapping(target = "currencyRates", source = "entity")
     CurrencyRatesResponseDTO toResponseDto(String baseCurrency, List<CurrencyRateEntity> entity);
+
+    @Mapping(target = "baseCurrency", source = "baseCurrency")
+    @Mapping(target = "rate", source = "dto.rate")
+    @Mapping(target = "currency", source = "dto.currencyName")
+    CurrencyRedisDTO toRedisDTO(
+        String baseCurrency, UpdateCurrencyRateDTO dto);
+
+    List<CurrencyRedisDTO> toRedisDTO(
+        String baseCurrency, List<UpdateCurrencyRateDTO> dto);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
