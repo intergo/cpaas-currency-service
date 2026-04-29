40e5e622ef8d73baa7e6dac6e7890e848121d78b
alexandros <alexandros.g@intergotelecom.com>
2026-04-29 15:30:01 +0300

    added new column

--- src/main/java/com/intergotelecom/model/CurrencyRateEntity.java
+++ src/main/java/com/intergotelecom/model/CurrencyRateEntity.java
@@ -1,7 +1,10 @@
 package com.intergotelecom.model;
 
+import com.intergotelecom.enums.RateProviderEnum;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,6 +43,10 @@ public class CurrencyRateEntity {
 
     @Column(name = "rate", nullable = false)
     private BigDecimal rate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rate_provider", nullable = false)
+    private RateProviderEnum rateProvider;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
