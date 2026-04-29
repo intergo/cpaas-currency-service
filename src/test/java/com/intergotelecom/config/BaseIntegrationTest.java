6d83b52f124b148a2ee52a082010d00d62c159a2
alexandros <alexandros.g@intergotelecom.com>
2026-04-29 14:04:45 +0300

    update base test

--- src/test/java/com/intergotelecom/config/BaseIntegrationTest.java
+++ src/test/java/com/intergotelecom/config/BaseIntegrationTest.java
@@ -3,5 +3,6 @@ package com.intergotelecom.config;
 import io.quarkus.test.common.QuarkusTestResource;
 
 @QuarkusTestResource(PostgresTestResource.class)
+@QuarkusTestResource(RedisTestResource.class)
 public abstract class BaseIntegrationTest {
 }
