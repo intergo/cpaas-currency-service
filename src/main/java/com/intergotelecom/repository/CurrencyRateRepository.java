631816c48af6b65eff728fd13edd7f5b9f1d3a5e
alexandros <alexandros.g@intergotelecom.com>
2026-04-29 15:52:43 +0300

    updated queries

--- src/main/java/com/intergotelecom/repository/CurrencyRateRepository.java
+++ src/main/java/com/intergotelecom/repository/CurrencyRateRepository.java
@@ -1,5 +1,6 @@
 package com.intergotelecom.repository;
 
+import com.intergotelecom.enums.RateProviderEnum;
 import com.intergotelecom.model.CurrencyRateEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -8,15 +9,18 @@ import java.util.Set;
 
 @ApplicationScoped
 public class CurrencyRateRepository implements PanacheRepository<CurrencyRateEntity> {
-    public List<CurrencyRateEntity> findByCurrencyAndBaseCurrency(
-        String baseCurrency, Set<String> currencyNames) {
-      return findByCurrencyAndBaseCurrency(baseCurrency, List.copyOf(currencyNames));
+    public List<CurrencyRateEntity> findByCurrencyBaseCurrencyAndProvider(
+        String baseCurrency, Set<String> currencyNames, RateProviderEnum rateProvider) {
+      return findByCurrencyBaseCurrencyAndProvider(
+          baseCurrency, List.copyOf(currencyNames), rateProvider);
     }
 
-    public List<CurrencyRateEntity> findByCurrencyAndBaseCurrency(
-        String baseCurrency, List<String> currencyNames) {
-      return list("baseCurrency.currencyName = ?1 and currency.currencyName in ?2",
-          baseCurrency, currencyNames);
+    public List<CurrencyRateEntity> findByCurrencyBaseCurrencyAndProvider(
+        String baseCurrency, List<String> currencyNames, RateProviderEnum rateProvider) {
+      return list("baseCurrency.currencyName = ?1 "
+              + "and currency.currencyName in ?2 "
+              + "and rateProvider = ?3",
+          baseCurrency, currencyNames, rateProvider);
     }
 
     public List<CurrencyRateEntity> findByBaseCurrency(String baseCurrencyName) {
