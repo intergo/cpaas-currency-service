36fec20f264b082114732d084f125132ccd6d8da
alexandros <alexandros.g@intergotelecom.com>
2026-04-29 17:09:38 +0300

    remove base currency from dto

--- src/main/java/com/intergotelecom/rest/dto/UpdateCurrencyRatesRequestDTO.java
+++ src/main/java/com/intergotelecom/rest/dto/UpdateCurrencyRatesRequestDTO.java
@@ -1,8 +1,6 @@
 package com.intergotelecom.rest.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -15,10 +13,6 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder(toBuilder = true)
 public class UpdateCurrencyRatesRequestDTO {
-    @NotBlank(message = "Base currency name cannot be blank")
-    @JsonProperty("base_currency_name")
-    private String baseCurrencyName;
-
     @Valid
     @NotEmpty(message = "Currency rate list cannot be empty")
     private List<UpdateCurrencyRateDTO> currencyRates;
